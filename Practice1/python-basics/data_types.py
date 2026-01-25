2e98e31ba40280a24ec443a15f32a05493faf8d5
Nuray <nurayserikkyzy538@gmail.com>
2026-01-25 11:38:17 +0500

    Add Python Data Types examples

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,38 @@
+
+x = 5       # int
+y = 3.14    # float
+z = -3255522
+print(x, y, z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+name = "Alice"
+greeting = 'Hello'
+print(name, greeting)
+print(type(name))
+print(type(greeting))
+
+
+first = "Python"
+second = "is cool"
+sentence = first + " " + second
+print("Объединение строк:", sentence)
+
+a = True
+b = False
+print(a, b)
+print(type(a))
+print(type(b))
+
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+print(type(fruits))
+
+
+
+numbers = (1, 2, 3)
+print(numbers)
+print(type(numbers))
