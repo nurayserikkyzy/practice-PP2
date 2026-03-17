efb70593f15621aafbbb75b55609f408fb1f190a
Nuray <nurayserikkyzy538@gmail.com>
2026-03-17 12:39:59 +0500

    Add Practice6 - file handling and built-in functions examples

--- Practice6/directory_management.py/create_list_dirs.py
+++ Practice6/directory_management.py/create_list_dirs.py
@@ -1,36 +1,14 @@
-# create_list_dirs.py
 import os
-import glob
-import shutil
 
-# Создаём вложенные папки
-os.makedirs("project_root/src/utils", exist_ok=True)
-os.makedirs("project_root/data/raw", exist_ok=True)
-os.makedirs("project_root/logs", exist_ok=True)
-print(" Nested directories created!")
+os.makedirs("folder1/folder2/folder3", exist_ok=True)
+print("Files in current directory")
+print(os.listdir("."))
 
-# Создаём файлы внутри
-open("project_root/src/utils/helpers.py", "w").close()
-open("project_root/data/raw/data.csv", "w").close()
-open("project_root/logs/app.log", "w").close()
+with open("z.txt","w") as f:
+    f.write("it is okay but it is not okay")
 
-# Показываем дерево папок
-print("\n Directory tree:")
-for root, dirs, files in os.walk("project_root"):
-    level = root.count(os.sep)
-    indent = "    " * level
-    print(f"{indent}📁 {os.path.basename(root)}/")
-    for file in files:
-        print(f"{indent}     {file}")
-
-# Ищем по расширению
-print("\n Python files:")
-for f in glob.glob("project_root/**/*.py", recursive=True):
-    print(f"  → {f}")
-
-print("\n Log files:")
-for f in glob.glob("project_root/**/*.log", recursive=True):
-    print(f"  → {f}")
-
-shutil.rmtree("project_root")
-print("\n Cleanup done.")
+    
+print("TXT files:")
+for file in os.listdir():
+    if file.endswith(".txt"):
+        print(file)
